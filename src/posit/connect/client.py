cfd96506727f03ded7543e4ae242619da8b97684
tdstein <taylor.steinberg@posit.co>
2024-02-05 11:51:53 -0500

    refactor: reduces config complexity

--- src/posit/connect/client.py
+++ src/posit/connect/client.py
@@ -1,11 +1,48 @@
+import os
+
 from requests import Session
 from typing import Optional
 
 from . import hooks
 
 from .auth import Auth
-from .config import ConfigBuilder
 from .users import Users
+
+
+def _get_api_key() -> str:
+    """Gets the API key from the environment variable 'CONNECT_API_KEY'.
+
+    Raises:
+        ValueError: if CONNECT_API_KEY is not set or invalid
+
+    Returns:
+        The API key
+    """
+    value = os.environ.get("CONNECT_API_KEY")
+    if value is None or value == "":
+        raise ValueError(
+            "Invalid value for 'CONNECT_API_KEY': Must be a non-empty string."
+        )
+    return value
+
+
+def _get_endpoint() -> str:
+    """Gets the endpoint from the environment variable 'CONNECT_SERVER'.
+
+    The `requests` library uses 'endpoint' instead of 'server'. We will use 'endpoint' from here forward for consistency.
+
+    Raises:
+        ValueError: if CONNECT_SERVER is not set or invalid.
+
+    Returns:
+        The endpoint.
+    """
+    value = os.environ.get("CONNECT_SERVER")
+    if value is None or value == "":
+        raise ValueError(
+            "Invalid value for 'CONNECT_SERVER': Must be a non-empty string."
+        )
+    return value
 
 
 class Client:
@@ -16,21 +53,9 @@ class Client:
         api_key: Optional[str] = None,
         endpoint: Optional[str] = None,
     ) -> None:
-        builder = ConfigBuilder()
-        if api_key:
-            builder.set_api_key(api_key)
-        if endpoint:
-            builder.set_endpoint(endpoint)
-        self._config = builder.build()
-
-        if self._config.api_key is None:
-            raise ValueError("Invalid value for 'api_key': Must be a non-empty string.")
-        if self._config.endpoint is None:
-            raise ValueError(
-                "Invalid value for 'endpoint': Must be a non-empty string."
-            )
-
+        self._api_key = api_key if api_key else _get_api_key()
+        self._endpoint = endpoint if endpoint else _get_endpoint()
         self._session = Session()
         self._session.hooks["response"].append(hooks.handle_errors)
-        self._session.auth = Auth(self._config.api_key)
-        self.users = Users(self._config.endpoint, self._session)
+        self._session.auth = Auth(self._api_key)
+        self.users = Users(self._endpoint, self._session)
