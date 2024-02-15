0a0d3fa8484cd843d08ef08b37e143d914ff73a6
Neal Richardson <neal.p.richardson@gmail.com>
2024-02-15 12:37:29 -0500

    fix: handle non-JSON error responses (#19)

--- src/posit/connect/hooks.py
+++ src/posit/connect/hooks.py
@@ -1,15 +1,19 @@
 from http.client import responses
-from requests import Response
+from requests import JSONDecodeError, Response
 
 from .errors import ClientError
 
 
 def handle_errors(response: Response, *args, **kwargs) -> Response:
-    if response.status_code >= 400 and response.status_code < 500:
-        data = response.json()
-        error_code = data["code"]
-        message = data["error"]
-        http_status = response.status_code
-        http_status_message = responses[http_status]
-        raise ClientError(error_code, message, http_status, http_status_message)
+    if response.status_code >= 400:
+        try:
+            data = response.json()
+            error_code = data["code"]
+            message = data["error"]
+            http_status = response.status_code
+            http_status_message = responses[http_status]
+            raise ClientError(error_code, message, http_status, http_status_message)
+        except JSONDecodeError:
+            # No JSON error message from Connect, so just raise
+            response.raise_for_status()
     return response
--- src/posit/connect/hooks_test.py
+++ src/posit/connect/hooks_test.py
@@ -1,7 +1,9 @@
 import pytest
 
+from requests import HTTPError, JSONDecodeError
 from unittest.mock import Mock
 
+from .errors import ClientError
 from .hooks import handle_errors
 
 
@@ -14,7 +16,14 @@ class TestHandleErrors:
     def test_client_error(self):
         response = Mock()
         response.status_code = 400
-        response.json = Mock()
-        response.json.return_value = {"code": 0, "error": "foobar"}
-        with pytest.raises(Exception):
+        response.json = Mock(return_value={"code": 0, "error": "foobar"})
+        with pytest.raises(ClientError):
+            handle_errors(response)
+
+    def test_client_error_not_json(self):
+        response = Mock()
+        response.status_code = 404
+        response.json = Mock(side_effect=JSONDecodeError("Not Found", "", 0))
+        response.raise_for_status = Mock(side_effect=HTTPError())
+        with pytest.raises(HTTPError):
             handle_errors(response)
