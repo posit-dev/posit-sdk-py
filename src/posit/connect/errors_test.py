6496f186b79c968e4989f6bdd6eba0dcb75d1e83
tdstein <taylor.steinberg@posit.co>
2024-02-20 14:23:11 -0500

    chore: splits unit tests by type

---
+++ src/posit/connect/errors_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from .errors import ClientError
+
+
+class TestClientError:
+    def test(self):
+        error_code = 0
+        error_message = "foo"
+        http_status = 404
+        http_message = "Foo Bar"
+        with pytest.raises(
+            ClientError, match=r"foo \(Error Code: 0, HTTP Status: 404 Foo Bar\)"
+        ):
+            raise ClientError(
+                error_code=error_code,
+                error_message=error_message,
+                http_status=http_status,
+                http_message=http_message,
+            )
