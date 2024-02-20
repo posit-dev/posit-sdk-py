9c5d78fcf1ad7293a4a90a1ffa6cf5ddf37d1cf5
Taylor Steinberg <taylor.steinberg@posit.co>
2024-02-20 14:51:49 -0500

    chore: splits unit tests by type (#37)

--- src/posit/connect/hooks_test.py
+++ src/posit/connect/hooks_test.py
@@ -1,31 +1,30 @@
-import io
-
 import pytest
 
-from requests import HTTPError, Response
+from unittest.mock import Mock, patch
 
 from .errors import ClientError
 from .hooks import handle_errors
 
 
-class TestHandleErrors:
-    def test(self):
-        response = Response()
-        response.status_code = 200
-        assert handle_errors(response) == response
+def test_success():
+    response = Mock()
+    response.status_code = 200
+    assert handle_errors(response) == response
 
-    def test_client_error(self):
-        response = Response()
-        response.status_code = 400
-        response.raw = io.BytesIO(b'{"code":0,"error":"foobar"}')
-        with pytest.raises(
-            ClientError, match=r"foobar \(Error Code: 0, HTTP Status: 400 Bad Request\)"
-        ):
-            handle_errors(response)
 
-    def test_client_error_not_json(self):
-        response = Response()
-        response.status_code = 404
-        response.raw = io.BytesIO(b"Plain text 404 Not Found")
-        with pytest.raises(HTTPError):
-            handle_errors(response)
+def test_client_error():
+    response = Mock()
+    response.status_code = 400
+    response.json = Mock(return_value={"code": 0, "error": "foobar"})
+    with pytest.raises(ClientError):
+        handle_errors(response)
+
+
+@patch("posit.connect.hooks.JSONDecodeError")
+def test_client_error_without_payload(JSONDecodeError):
+    response = Mock()
+    response.status_code = 404
+    response.json = Mock(side_effect=JSONDecodeError())
+    response.raise_for_status = Mock(side_effect=Exception())
+    with pytest.raises(Exception):
+        handle_errors(response)
