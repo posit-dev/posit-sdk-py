56b1bd330e7ca76c8ca81b4854e99898fe9bd857
David Kegley <david.kegley@posit.co>
2024-02-22 10:41:46 -0500

    Add custom credential provider implementation

--- src/posit/connect/external/databricks.py
+++ src/posit/connect/external/databricks.py
@@ -1,11 +1,43 @@
-from typing import Dict, Optional
+import abc
+from typing import Callable, Dict, Optional
 
 from ..oauth import OAuthIntegration
 
-from databricks.sdk.core import Config, HeaderFactory, credentials_provider
+
+HeaderFactory = Callable[[], Dict[str, str]]
+
+# https://github.com/databricks/databricks-sdk-py/blob/v0.20.0/databricks/sdk/credentials_provider.py
+# https://github.com/databricks/databricks-sql-python/blob/v3.1.0/src/databricks/sql/auth/authenticators.py
+# In order to keep compatibility with the Databricks SDK
+class CredentialsProvider(abc.ABC):
+    """ CredentialsProvider is the protocol (call-side interface)
+     for authenticating requests to Databricks REST APIs"""
+
+    @abc.abstractmethod
+    def auth_type(self) -> str:
+        ...
+
+    @abc.abstractmethod
+    def __call__(self, *args, **kwargs) -> HeaderFactory:
+        ...
 
 
-def viewer_credentials_provider(posit_oauth: OAuthIntegration, content_identity: Optional[str]):
+class PositOAuthIntegrationCredentialsProvider(CredentialsProvider):
+    def __init__(self, posit_oauth: OAuthIntegration, content_identity: str):
+        self.posit_oauth = posit_oauth
+        self.content_identity = content_identity
+
+    def auth_type(self) -> str:
+        return "posit-oauth-integration"
+
+    def __call__(self, *args, **kwargs) -> HeaderFactory:
+        def inner() -> Dict[str, str]:
+            access_token = self.posit_oauth.get_credentials(self.content_identity).json()['access_token']
+            return {"Authorization": f"Bearer {access_token}"}
+        return inner
+
+
+def viewer_credentials_provider(posit_oauth: OAuthIntegration, content_identity: Optional[str]) -> Optional[CredentialsProvider]:
 
     # If the content is not running on Connect then viewer auth should
     # fall back to the locally configured credentials hierarchy
@@ -17,59 +49,8 @@ def viewer_credentials_provider(posit_oauth: OAuthIntegration, content_identity:
     if content_identity is None:
         raise Exception("The content-identity-token is required for viewer authentication.")
 
-    #@credentials_provider
-    def custom_credentials_provider(*args):
-        def inner() -> Dict[str, str]:
-            access_token = posit_oauth.get_credentials(content_identity).json()['access_token']
-            return {"Authorization": f"Bearer {access_token}"}
-        return inner
-    return custom_credentials_provider
+    return PositOAuthIntegrationCredentialsProvider(posit_oauth, content_identity)
 
 
-def viewer_sql_credentials_provider(posit_oauth: OAuthIntegration, content_identity: Optional[str]):
-
-    # If the content is not running on Connect then viewer auth should
-    # fall back to the locally configured credentials hierarchy
-    if posit_oauth.client.is_local():
-        return None
-
-    # If the content-identity-token wasn't provided and we're running on Connect then we raise an exception.
-    # content_identity is required to impersonate the viewer.
-    if content_identity is None:
-        raise Exception("The content-identity-token is required for viewer authentication.")
-
-    def custom_credentials_provider():
-        def inner() -> Dict[str, str]:
-            access_token = posit_oauth.get_credentials(content_identity).json()['access_token']
-            return {"Authorization": f"Bearer {access_token}"}
-        return inner
-    return custom_credentials_provider
-
-
-def viewer_sdk_credentials_provider(posit_oauth: OAuthIntegration, content_identity: Optional[str]):
-
-    # If the content is not running on Connect then viewer auth should
-    # fall back to the locally configured credentials hierarchy
-    if posit_oauth.client.is_local():
-        return None
-
-    # If the content-identity-token wasn't provided and we're running on Connect then we raise an exception.
-    # content_identity is required to impersonate the viewer.
-    if content_identity is None:
-        raise Exception("The content-identity-token is required for viewer authentication.")
-
-    @credentials_provider("custom", ["host"])
-    def custom_credentials_provider(cfg: Config) -> HeaderFactory:
-        def inner() -> Dict[str, str]:
-            access_token = posit_oauth.get_credentials(content_identity).json()['access_token']
-            return {"Authorization": f"Bearer {access_token}"}
-        return inner
-    return custom_credentials_provider
-
-
-def service_account_sql_credentials_provider(posit_oauth: OAuthIntegration):
-    raise NotImplemented
-
-
-def service_account_sdk_credentials_provider(posit_oauth: OAuthIntegration):
+def service_account_credentials_provider(posit_oauth: OAuthIntegration):
     raise NotImplemented
