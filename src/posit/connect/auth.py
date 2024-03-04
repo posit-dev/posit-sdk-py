1ad93c63ad3c11f4f1a1ac51157ee836dba6c41c
Taylor Steinberg <taylor@steinberg.xyz>
2024-03-04 18:32:08 +0000

    style: marks config as public

--- src/posit/connect/auth.py
+++ src/posit/connect/auth.py
@@ -6,8 +6,8 @@ from .config import Config
 
 class Auth(AuthBase):
     def __init__(self, config: Config) -> None:
-        self._config = config
+        self.config = config
 
     def __call__(self, r: PreparedRequest) -> PreparedRequest:
-        r.headers["Authorization"] = f"Key {self._config.api_key}"
+        r.headers["Authorization"] = f"Key {self.config.api_key}"
         return r
