a6f21061486e29d37ab53fd2efebf4caf2e6ae2d
Pablo Bianco Lascaray <pablo@rstudio.com>
2024-03-05 16:41:44 -0500

    Refactored code to simplify layout.

--- examples/connect/shiny-python/app.py
+++ examples/connect/shiny-python/app.py
@@ -22,21 +22,6 @@ app_ui = ui.page_fluid(
     ui.output_data_frame("result")
 )
 
-def get_databricks_user_info(credentials_provider):
-    """
-    Use the Databricks SDK to get the current user's information.
-    """
-
-    if os.getenv("CONNECT_SERVER"):
-        cfg = Config(
-            host=DATABRICKS_HOST_URL,
-            credentials_provider=credentials_provider
-        )
-    else:
-        cfg = Config(host=DATABRICKS_HOST_URL, token=DATABRICKS_PAT)
-
-    return CurrentUserAPI(ApiClient(cfg)).me()
-
 def server(input: Inputs, output: Outputs, session: Session):
     """
     Shiny for Python example application that shows user information and 
@@ -45,7 +30,6 @@ def server(input: Inputs, output: Outputs, session: Session):
 
     session_token = session.http_conn.headers.get('Posit-Connect-User-Session-Token')
     credentials_provider = viewer_credentials_provider(user_session_token=session_token)
-    databricks_user_info = get_databricks_user_info(credentials_provider=credentials_provider)
 
     @render.data_frame
     def result():
@@ -64,6 +48,10 @@ def server(input: Inputs, output: Outputs, session: Session):
 
     @render.text
     def text():
+        cfg = Config(
+            host=DATABRICKS_HOST_URL, credentials_provider=credentials_provider
+        )
+        databricks_user_info = CurrentUserAPI(ApiClient(cfg)).me()
         return f"Hello, {databricks_user_info.display_name}!"
 
 app = App(app_ui, server)
