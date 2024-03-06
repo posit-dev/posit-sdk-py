868027c9485db11034a1e71efee64d830473147b
tdstein <taylor.steinberg@posit.co>
2024-03-06 15:01:41 -0500

    chore: applies formatting

--- examples/connect/fastapi/app.py
+++ examples/connect/fastapi/app.py
@@ -11,15 +11,16 @@ from typing import Annotated
 from fastapi import FastAPI, Header, Depends
 from fastapi.responses import JSONResponse
 
-DATABRICKS_HOST=os.getenv("DATABRICKS_HOST")
+DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
 DATABRICKS_HOST_URL = f"https://{DATABRICKS_HOST}"
 SQL_HTTP_PATH = os.getenv("DATABRICKS_PATH")
 
 rows = None
 app = FastAPI()
 
+
 async def get_token(
-    posit_connect_user_session_token: Annotated[str | None, Header()] = None
+    posit_connect_user_session_token: Annotated[str | None, Header()] = None,
 ) -> dict | None:
     """
     Get the token from the Posit-Connect-User-Session-Token header.
@@ -27,6 +28,7 @@ async def get_token(
     if posit_connect_user_session_token is None:
         return None
     return posit_connect_user_session_token
+
 
 @app.get("/fares")
 async def get_fares(session_token=Depends(get_token)) -> JSONResponse:
@@ -41,10 +43,12 @@ async def get_fares(session_token=Depends(get_token)) -> JSONResponse:
     if rows is None:
         query = "SELECT * FROM samples.nyctaxi.trips LIMIT 10;"
 
-        with sql.connect(server_hostname=DATABRICKS_HOST,
-                        http_path=SQL_HTTP_PATH,
-                        auth_type="databricks-oauth",
-                        credentials_provider=credentials_provider) as connection:
+        with sql.connect(
+            server_hostname=DATABRICKS_HOST,
+            http_path=SQL_HTTP_PATH,
+            auth_type="databricks-oauth",
+            credentials_provider=credentials_provider,
+        ) as connection:
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 rows = cursor.fetchall()
