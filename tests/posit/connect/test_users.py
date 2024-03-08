2f72326a3a2cc3d93ab4ceb598926772fdcf9a20
Neal Richardson <neal.p.richardson@gmail.com>
2024-03-08 09:45:34 -0500

    Move user API responses to JSON files and add helper to read them

--- tests/posit/connect/test_users.py
+++ tests/posit/connect/test_users.py
@@ -3,6 +3,8 @@ import responses
 
 from posit.connect.client import Client
 
+from .api import load_mock
+
 
 class TestUsers:
     @responses.activate
@@ -14,38 +16,7 @@ class TestUsers:
                     {"page_size": 2, "page_number": 1}
                 )
             ],
-            json={
-                "results": [
-                    {
-                        "email": "alice@connect.example",
-                        "username": "al",
-                        "first_name": "Alice",
-                        "last_name": "User",
-                        "user_role": "publisher",
-                        "created_time": "2017-08-08T15:24:32Z",
-                        "updated_time": "2023-03-02T20:25:06Z",
-                        "active_time": "2018-05-09T16:58:45Z",
-                        "confirmed": True,
-                        "locked": False,
-                        "guid": "a01792e3-2e67-402e-99af-be04a48da074",
-                    },
-                    {
-                        "email": "bob@connect.example",
-                        "username": "robert",
-                        "first_name": "Bob",
-                        "last_name": "Loblaw",
-                        "user_role": "publisher",
-                        "created_time": "2023-01-06T19:47:29Z",
-                        "updated_time": "2023-05-05T19:08:45Z",
-                        "active_time": "2023-05-05T20:29:11Z",
-                        "confirmed": True,
-                        "locked": False,
-                        "guid": "87c12c08-11cd-4de1-8da3-12a7579c4998",
-                    },
-                ],
-                "current_page": 1,
-                "total": 3,
-            },
+            json=load_mock("v1/users?page_number=1&page_size=2.json"),
         )
         responses.get(
             "https://connect.example/__api__/v1/users",
@@ -54,25 +25,7 @@ class TestUsers:
                     {"page_size": 2, "page_number": 2}
                 )
             ],
-            json={
-                "results": [
-                    {
-                        "email": "carlos@connect.example",
-                        "username": "carlos12",
-                        "first_name": "Carlos",
-                        "last_name": "User",
-                        "user_role": "publisher",
-                        "created_time": "2019-09-09T15:24:32Z",
-                        "updated_time": "2022-03-02T20:25:06Z",
-                        "active_time": "2020-05-11T16:58:45Z",
-                        "confirmed": True,
-                        "locked": False,
-                        "guid": "20a79ce3-6e87-4522-9faf-be24228800a4",
-                    },
-                ],
-                "current_page": 2,
-                "total": 3,
-            },
+            json=load_mock("v1/users?page_number=2&page_size=2.json"),
         )
 
         con = Client(api_key="12345", url="https://connect.example/")
@@ -112,19 +65,7 @@ class TestUsers:
     def test_users_get(self):
         responses.get(
             "https://connect.example/__api__/v1/users/20a79ce3-6e87-4522-9faf-be24228800a4",
-            json={
-                "email": "carlos@connect.example",
-                "username": "carlos12",
-                "first_name": "Carlos",
-                "last_name": "User",
-                "user_role": "publisher",
-                "created_time": "2019-09-09T15:24:32Z",
-                "updated_time": "2022-03-02T20:25:06Z",
-                "active_time": "2020-05-11T16:58:45Z",
-                "confirmed": True,
-                "locked": False,
-                "guid": "20a79ce3-6e87-4522-9faf-be24228800a4",
-            },
+            json=load_mock("v1/users/20a79ce3-6e87-4522-9faf-be24228800a4.json"),
         )
 
         con = Client(api_key="12345", url="https://connect.example/")
