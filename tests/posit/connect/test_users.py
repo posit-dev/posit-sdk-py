e44c3528e7bb232d6222a971e0803b94aea30ed5
tdstein <taylor.steinberg@posit.co>
2024-03-19 15:58:13 -0400

    refactor: modifies paginator

--- tests/posit/connect/test_users.py
+++ tests/posit/connect/test_users.py
@@ -3,7 +3,6 @@ import pandas as pd
 import pytest
 import responses
 
-from requests import HTTPError
 
 from posit.connect.client import Client
 from posit.connect.users import User
@@ -100,7 +99,7 @@ class TestUsers:
             "https://connect.example/__api__/v1/users",
             match=[
                 responses.matchers.query_param_matcher(
-                    {"page_size": 2, "page_number": 1}
+                    {"page_size": 500, "page_number": 1}
                 )
             ],
             json=load_mock("v1/users?page_number=1&page_size=2.json"),
@@ -109,14 +108,14 @@ class TestUsers:
             "https://connect.example/__api__/v1/users",
             match=[
                 responses.matchers.query_param_matcher(
-                    {"page_size": 2, "page_number": 2}
+                    {"page_size": 500, "page_number": 2}
                 )
             ],
             json=load_mock("v1/users?page_number=2&page_size=2.json"),
         )
 
         con = Client(api_key="12345", url="https://connect.example/")
-        all_users = con.users.find(page_size=2)
+        all_users = con.users.find()
         assert len(all_users) == 3
 
         df = pd.DataFrame(all_users)
@@ -143,7 +142,7 @@ class TestUsers:
             "https://connect.example/__api__/v1/users",
             match=[
                 responses.matchers.query_param_matcher(
-                    {"page_size": 2, "page_number": 1}
+                    {"page_size": 500, "page_number": 1}
                 )
             ],
             json=load_mock("v1/users?page_number=1&page_size=2.json"),
@@ -152,51 +151,15 @@ class TestUsers:
             "https://connect.example/__api__/v1/users",
             match=[
                 responses.matchers.query_param_matcher(
-                    {"page_size": 2, "page_number": 2}
+                    {"page_size": 500, "page_number": 2}
                 )
             ],
             json=load_mock("v1/users?page_number=2&page_size=2.json"),
         )
 
         con = Client(api_key="12345", url="https://connect.example/")
-        c = con.users.find_one(lambda u: u.first_name == "Carlos", page_size=2)
-        # Can't isinstance(c, User) bc inherits TypedDict (cf. #23)
-        assert c.username == "carlos12"
-
-        # Now test that if not found, it returns None
-        assert (
-            con.users.find_one(lambda u: u.first_name == "Ringo", page_size=2) is None
-        )
-
-    @responses.activate
-    def test_users_find_one_only_gets_necessary_pages(self):
-        responses.get(
-            "https://connect.example/__api__/v1/users",
-            match=[
-                responses.matchers.query_param_matcher(
-                    {"page_size": 2, "page_number": 1}
-                )
-            ],
-            json=load_mock("v1/users?page_number=1&page_size=2.json"),
-        )
-        # Make page 2 return an error so we can prove that we're quitting
-        # when we find the user
-        responses.get(
-            "https://connect.example/__api__/v1/users",
-            match=[
-                responses.matchers.query_param_matcher(
-                    {"page_size": 2, "page_number": 2}
-                )
-            ],
-            status=500,
-        )
-
-        con = Client(api_key="12345", url="https://connect.example/")
-        bob = con.users.find_one(lambda u: u.first_name == "Bob", page_size=2)
-        assert bob.username == "robert"
-        # This errors because we have to go past the first page
-        with pytest.raises(HTTPError, match="500 Server Error"):
-            con.users.find_one(lambda u: u.first_name == "Carlos", page_size=2)
+        c = con.users.find_one()
+        assert c.username == "al"
 
     @responses.activate
     def test_users_get(self):
