6976c132fc36b7474f9cce6c036dce74292a7564
tdstein <taylor.steinberg@posit.co>
2024-03-22 10:15:22 -0400

    refactor: inject config instead of url into resource

--- src/posit/connect/client.py
+++ src/posit/connect/client.py
@@ -50,11 +50,7 @@ class Client:
     def me(self) -> User:
         url = urls.append_path(self.config.url, "v1/user")
         response = self.session.get(url)
-        body = response.json()
-        # Build the canonical URL for the User based on its GUID
-        guid = body["guid"]
-        url = urls.append_path(self.config.url, f"v1/users/{guid}")
-        return User(self.session, url, **body)
+        return User(self.config, self.session, **response.json())
 
     @property
     def oauth(self) -> OAuthIntegration:
