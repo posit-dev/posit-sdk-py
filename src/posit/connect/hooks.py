b52e4244e866e704c3019fd10e75e9a9132565b2
Neal Richardson <neal.p.richardson@gmail.com>
2024-05-28 10:50:47 -0400

    feat: Check for X-Deprecated-Endpoint header (#195)

--- src/posit/connect/hooks.py
+++ src/posit/connect/hooks.py
@@ -1,3 +1,5 @@
+import warnings
+
 from http.client import responses
 from requests import JSONDecodeError, Response
 
@@ -12,10 +14,27 @@ def handle_errors(response: Response, *args, **kwargs) -> Response:
             message = data["error"]
             http_status = response.status_code
             http_status_message = responses[http_status]
-            raise ClientError(
-                error_code, message, http_status, http_status_message
-            )
+            raise ClientError(error_code, message, http_status, http_status_message)
         except JSONDecodeError:
             # No JSON error message from Connect, so just raise
             response.raise_for_status()
+    return response
+
+
+def check_for_deprecation_header(response: Response, *args, **kwargs) -> Response:
+    """
+    Check for deprecation warnings from the server.
+
+    You might get these if you've upgraded the Connect server but not posit-sdk.
+    posit-sdk will make the right request based on the version of the server,
+    but if you have an old version of the package, it won't know the new URL
+    to request.
+    """
+    if "X-Deprecated-Endpoint" in response.headers:
+        msg = (
+            response.url
+            + " is deprecated and will be removed in a future version of Connect."
+            + " Please upgrade `posit-sdk` in order to use the new APIs."
+        )
+        warnings.warn(msg, DeprecationWarning)
     return response
