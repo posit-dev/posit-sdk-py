0017e23f3ccd124a0cd0fb7d3a2b385586b8e1cc
tdstein <taylor.steinberg@posit.co>
2024-06-05 13:51:40 -0400

    build: add parallel integration tests

--- integration/tests/posit/connect/test_client.py
+++ integration/tests/posit/connect/test_client.py
@@ -1,7 +1,13 @@
 import os
+import pytest
 
 from posit import connect
 
+
+@pytest.mark.skipif(
+    os.environ.get("CONNECT_VERSION") is None,
+    reason="requires CONNECT_VERSION environment variable",
+)
 def test_version():
     client = connect.Client()
     assert client.version == os.environ.get("CONNECT_VERSION")
