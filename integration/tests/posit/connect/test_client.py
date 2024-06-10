52e35e360a6b2e687e27b5b5ad45c4e892d0763a
Taylor Steinberg <taylor.steinberg@posit.co>
2024-06-10 09:43:09 -0400

    build: add Connect preview to integration test suite (#213)

--- integration/tests/posit/connect/test_client.py
+++ integration/tests/posit/connect/test_client.py
@@ -4,10 +4,6 @@ import pytest
 from posit import connect
 
 
-@pytest.mark.skipif(
-    os.environ.get("CONNECT_VERSION") is None,
-    reason="requires CONNECT_VERSION environment variable",
-)
 def test_version():
     client = connect.Client()
-    assert client.version == os.environ.get("CONNECT_VERSION")
+    assert client.version
