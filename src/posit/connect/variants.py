1a5af183e5893f50fe89b5f7e195a1e8bcfc7a17
tdstein <taylor.steinberg@posit.co>
2024-07-17 14:45:41 -0400

    --wip-- [skip ci]

--- src/posit/connect/variants.py
+++ src/posit/connect/variants.py
@@ -4,9 +4,10 @@ from requests import Session
 from . import urls
 from .config import Config
 from .resources import Resource, Resources
+from .tasks import Task
 
 
-class _Variant(Resource):
+class Variant(Resource):
     @property
     def id(self) -> str:
         return self["id"]
@@ -15,24 +16,25 @@ class _Variant(Resource):
     def is_default(self) -> bool:
         return self.get("is_default", False)
 
-    def render(self) -> None:
+    def render(self) -> Task:
         path = f"variants/{self.id}/render"
         url = urls.append(self.config.url, path)
-        self.session.post(url)
+        response = self.session.post(url)
+        return Task(self.config, self.session, **response.json())
 
 
-class _Variants(Resources):
+class Variants(Resources):
     def __init__(
         self, config: Config, session: Session, content_guid: str
     ) -> None:
         super().__init__(config, session)
         self.content_guid = content_guid
 
-    def find(self) -> List[_Variant]:
+    def find(self) -> List[Variant]:
         path = f"applications/{self.content_guid}/variants"
         url = urls.append(self.config.url, path)
         response = self.session.get(url)
         results = response.json()
         return [
-            _Variant(self.config, self.session, **result) for result in results
+            Variant(self.config, self.session, **result) for result in results
         ]
