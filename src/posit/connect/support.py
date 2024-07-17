a00bba5b176329be1419d571d5ee68bbb1e3c657
tdstein <taylor.steinberg@posit.co>
2024-07-17 14:24:15 -0400

    --wip-- [skip ci]

--- src/posit/connect/support.py
+++
@@ -1,6 +0,0 @@
-import urllib.parse
-
-def create_github_issue_link(issue_title: str, issue_body: str):
-    title = urllib.parse.quote(title)
-    body = urllib.parse.quote(body)
-    return f"https://github.com/posit-dev/posit-sdk-py/issues/new?title={title}&body={body}"
