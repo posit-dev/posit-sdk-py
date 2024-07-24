0eae9a195c1a82adc820a30a865cdd0b7129ae3a
tdstein <taylor.steinberg@posit.co>
2024-07-24 09:38:50 -0400

    mark context attributes as Final

--- src/posit/connect/context.py
+++ src/posit/connect/context.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 import requests
 
 
@@ -5,6 +7,6 @@ class Context:
     def __init__(
         self, *, api_key: str, session: requests.Session, url: str
     ) -> None:
-        self.api_key = api_key
-        self.session = session
-        self.url = url
+        self.api_key: Final[str] = api_key
+        self.session: Final[requests.Session] = session
+        self.url: Final[str] = url
