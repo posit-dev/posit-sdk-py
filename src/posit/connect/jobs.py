7c8acce9ddb7e103d14c5bb0ce9b8794682b0f5d
Taylor Steinberg <taylor@steinberg.xyz>
2024-10-29 21:17:01 -0400

    feat: add jobs (#314)
    Adds job support.
    
    The mixin pattern is continued, and a 'jobs' property is added to
    `ContentItem.`
    
    A new abstraction around resources called `Active` is introduced. This
    abstraction begins transitioning to a more idiomatic approach to
    interacting with collections. Similar to how printing a `Resource` shows
    a `dict`, the `ActiveSequence` shows a familiar `List[dict]`
    representation. The `ActiveFinderMethods` supports the `find` and
    `find_by` methods. This is inspired by this Ruby on Rails module
    https://api.rubyonrails.org/classes/ActiveRecord/FinderMethods.html.
    Ideally, this separation of concerns provides a straightforward way to
    compose bindings in the future and reduces the amount of duplication
    across implementations.
    
    Additional background may be found here
    https://github.com/tdstein/active
    
    Related to #310 
    Resolves #306

---
+++ src/posit/connect/jobs.py
@@ -0,0 +1,292 @@
+from typing import Literal, Optional, TypedDict, overload
+
+from typing_extensions import NotRequired, Required, Unpack
+
+from .resources import Active, ActiveFinderMethods, ActiveSequence, Resource
+
+JobTag = Literal[
+    "unknown",
+    "build_report",
+    "build_site",
+    "build_jupyter",
+    "packrat_restore",
+    "python_restore",
+    "configure_report",
+    "run_app",
+    "run_api",
+    "run_tensorflow",
+    "run_python_api",
+    "run_dash_app",
+    "run_streamlit",
+    "run_bokeh_app",
+    "run_fastapi_app",
+    "run_pyshiny_app",
+    "render_shiny",
+    "run_voila_app",
+    "testing",
+    "git",
+    "val_py_ext_pkg",
+    "val_r_ext_pkg",
+    "val_r_install",
+]
+
+
+class Job(Active):
+    class _Job(TypedDict):
+        # Identifiers
+        id: Required[str]
+        """A unique identifier for the job."""
+
+        ppid: Required[Optional[str]]
+        """Identifier of the parent process."""
+
+        pid: Required[str]
+        """Identifier of the process running the job."""
+
+        key: Required[str]
+        """A unique key to identify this job."""
+
+        remote_id: Required[Optional[str]]
+        """Identifier for off-host execution configurations."""
+
+        app_id: Required[str]
+        """Identifier of the parent content associated with the job."""
+
+        variant_id: Required[str]
+        """Identifier of the variant responsible for the job."""
+
+        bundle_id: Required[str]
+        """Identifier of the content bundle linked to the job."""
+
+        # Timestamps
+        start_time: Required[str]
+        """RFC3339 timestamp indicating when the job started."""
+
+        end_time: Required[Optional[str]]
+        """RFC3339 timestamp indicating when the job finished."""
+
+        last_heartbeat_time: Required[str]
+        """RFC3339 timestamp of the last recorded activity for the job."""
+
+        queued_time: Required[Optional[str]]
+        """RFC3339 timestamp when the job was added to the queue."""
+
+        # Status and Exit Information
+        status: Required[Literal[0, 1, 2]]
+        """Current status. Options are 0 (Active), 1 (Finished), and 2 (Finalized)"""
+
+        exit_code: Required[Optional[int]]
+        """The job's exit code, available after completion."""
+
+        # Environment Information
+        hostname: Required[str]
+        """Name of the node processing the job."""
+
+        cluster: Required[Optional[str]]
+        """Location where the job runs, either 'Local' or the cluster name."""
+
+        image: Required[Optional[str]]
+        """Location of the content in clustered environments."""
+
+        run_as: Required[str]
+        """UNIX user responsible for executing the job."""
+
+        # Queue and Scheduling Information
+        queue_name: Required[Optional[str]]
+        """Name of the queue processing the job, relevant for scheduled reports."""
+
+        # Job Metadata
+        tag: Required[JobTag]
+        """A tag categorizing the job type. Options are build_jupyter, build_report, build_site, configure_report, git, packrat_restore, python_restore, render_shiny, run_api, run_app, run_bokeh_app, run_dash_app, run_fastapi_app, run_pyshiny_app, run_python_api, run_streamlit, run_tensorflow, run_voila_app, testing, unknown, val_py_ext_pkg, val_r_ext_pkg, and val_r_install."""
+
+    def __init__(self, ctx, parent: Active, **kwargs: Unpack[_Job]):
+        super().__init__(ctx, parent, **kwargs)
+        self._parent = parent
+
+    @property
+    def _endpoint(self) -> str:
+        return self._ctx.url + f"v1/content/{self._parent['guid']}/jobs/{self['key']}"
+
+    def destroy(self) -> None:
+        """Destroy the job.
+
+        Submit a request to kill the job.
+
+        Warnings
+        --------
+        This operation is irreversible.
+
+        Note
+        ----
+        This action requires administrator, owner, or collaborator privileges.
+        """
+        self._ctx.session.delete(self._endpoint)
+
+
+class Jobs(
+    ActiveFinderMethods[Job],
+    ActiveSequence[Job],
+):
+    def __init__(self, ctx, parent: Active, uid="key"):
+        """A collection of jobs.
+
+        Parameters
+        ----------
+        ctx : Context
+            The context containing the HTTP session used to interact with the API.
+        parent : Active
+            Parent resource for maintaining hierarchical relationships
+        uid : str, optional
+            The default field name used to uniquely identify records, by default "key"
+        """
+        super().__init__(ctx, parent, uid)
+        self._parent = parent
+
+    @property
+    def _endpoint(self) -> str:
+        return self._ctx.url + f"v1/content/{self._parent['guid']}/jobs"
+
+    def _create_instance(self, **kwargs) -> Job:
+        """Creates a `Job` instance.
+
+        Returns
+        -------
+        Job
+        """
+        return Job(self._ctx, self._parent, **kwargs)
+
+    class _FindByRequest(TypedDict, total=False):
+        # Identifiers
+        id: Required[str]
+        """A unique identifier for the job."""
+
+        ppid: NotRequired[Optional[str]]
+        """Identifier of the parent process."""
+
+        pid: NotRequired[str]
+        """Identifier of the process running the job."""
+
+        key: NotRequired[str]
+        """A unique key to identify this job."""
+
+        remote_id: NotRequired[Optional[str]]
+        """Identifier for off-host execution configurations."""
+
+        app_id: NotRequired[str]
+        """Identifier of the parent content associated with the job."""
+
+        variant_id: NotRequired[str]
+        """Identifier of the variant responsible for the job."""
+
+        bundle_id: NotRequired[str]
+        """Identifier of the content bundle linked to the job."""
+
+        # Timestamps
+        start_time: NotRequired[str]
+        """RFC3339 timestamp indicating when the job started."""
+
+        end_time: NotRequired[Optional[str]]
+        """RFC3339 timestamp indicating when the job finished."""
+
+        last_heartbeat_time: NotRequired[str]
+        """RFC3339 timestamp of the last recorded activity for the job."""
+
+        queued_time: NotRequired[Optional[str]]
+        """RFC3339 timestamp when the job was added to the queue."""
+
+        # Status and Exit Information
+        status: NotRequired[Literal[0, 1, 2]]
+        """Current status. Options are 0 (Active), 1 (Finished), and 2 (Finalized)"""
+
+        exit_code: NotRequired[Optional[int]]
+        """The job's exit code, available after completion."""
+
+        # Environment Information
+        hostname: NotRequired[str]
+        """Name of the node processing the job."""
+
+        cluster: NotRequired[Optional[str]]
+        """Location where the job runs, either 'Local' or the cluster name."""
+
+        image: NotRequired[Optional[str]]
+        """Location of the content in clustered environments."""
+
+        run_as: NotRequired[str]
+        """UNIX user responsible for executing the job."""
+
+        # Queue and Scheduling Information
+        queue_name: NotRequired[Optional[str]]
+        """Name of the queue processing the job, relevant for scheduled reports."""
+
+        # Job Metadata
+        tag: NotRequired[JobTag]
+        """A tag categorizing the job type. Options are build_jupyter, build_report, build_site, configure_report, git, packrat_restore, python_restore, render_shiny, run_api, run_app, run_bokeh_app, run_dash_app, run_fastapi_app, run_pyshiny_app, run_python_api, run_streamlit, run_tensorflow, run_voila_app, testing, unknown, val_py_ext_pkg, val_r_ext_pkg, and val_r_install."""
+
+    @overload
+    def find_by(self, **conditions: Unpack[_FindByRequest]) -> Optional[Job]:
+        """Finds the first record matching the specified conditions.
+
+        There is no implied ordering so if order matters, you should specify it yourself.
+
+        Parameters
+        ----------
+        id : str, not required
+            A unique identifier for the job.
+        ppid : Optional[str], not required
+            Identifier of the parent process.
+        pid : str, not required
+            Identifier of the process running the job.
+        key : str, not required
+            A unique key to identify this job.
+        remote_id : Optional[str], not required
+            Identifier for off-host execution configurations.
+        app_id : str, not required
+            Identifier of the parent content associated with the job.
+        variant_id : str, not required
+            Identifier of the variant responsible for the job.
+        bundle_id : str, not required
+            Identifier of the content bundle linked to the job.
+        start_time : str, not required
+            RFC3339 timestamp indicating when the job started.
+        end_time : Optional[str], not required
+            RFC3339 timestamp indicating when the job finished.
+        last_heartbeat_time : str, not required
+            RFC3339 timestamp of the last recorded activity for the job.
+        queued_time : Optional[str], not required
+            RFC3339 timestamp when the job was added to the queue.
+        status : int, not required
+            Current status. Options are 0 (Active), 1 (Finished), and 2 (Finalized)
+        exit_code : Optional[int], not required
+            The job's exit code, available after completion.
+        hostname : str, not required
+            Name of the node processing the job.
+        cluster : Optional[str], not required
+            Location where the job runs, either 'Local' or the cluster name.
+        image : Optional[str], not required
+            Location of the content in clustered environments.
+        run_as : str, not required
+            UNIX user responsible for executing the job.
+        queue_name : Optional[str], not required
+            Name of the queue processing the job, relevant for scheduled reports.
+        tag : JobTag, not required
+            A tag categorizing the job type. Options are build_jupyter, build_report, build_site, configure_report, git, packrat_restore, python_restore, render_shiny, run_api, run_app, run_bokeh_app, run_dash_app, run_fastapi_app, run_pyshiny_app, run_python_api, run_streamlit, run_tensorflow, run_voila_app, testing, unknown, val_py_ext_pkg, val_r_ext_pkg, and val_r_install.
+
+        Returns
+        -------
+        Optional[Job]
+        """
+        ...
+
+    @overload
+    def find_by(self, **conditions): ...
+
+    def find_by(self, **conditions) -> Optional[Job]:
+        return super().find_by(**conditions)
+
+
+class JobsMixin(Active, Resource):
+    """Mixin class to add a jobs attribute to a resource."""
+
+    def __init__(self, ctx, **kwargs):
+        super().__init__(ctx, **kwargs)
+        self.jobs = Jobs(ctx, self)
--- src/posit/connect/resources.py
+++ src/posit/connect/resources.py
@@ -1,8 +1,12 @@
 import warnings
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Any, Generic, List, Optional, Sequence, TypeVar, overload
 
 import requests
+from typing_extensions import Self
 
+from .context import Context
 from .urls import Url
 
 
@@ -43,3 +47,196 @@ class Resource(dict):
 class Resources:
     def __init__(self, params: ResourceParameters) -> None:
         self.params = params
+
+
+class Active(ABC, Resource):
+    def __init__(self, ctx: Context, parent: Optional["Active"] = None, **kwargs):
+        """A base class representing an active resource.
+
+        Extends the `Resource` class and provides additional functionality for via the session context and an optional parent resource.
+
+        Parameters
+        ----------
+        ctx : Context
+            The context object containing the session and URL for API interactions.
+        parent : Optional[Active], optional
+            An optional parent resource that establishes a hierarchical relationship, by default None.
+        **kwargs : dict
+            Additional keyword arguments passed to the parent `Resource` class.
+        """
+        params = ResourceParameters(ctx.session, ctx.url)
+        super().__init__(params, **kwargs)
+        self._ctx = ctx
+        self._parent = parent
+
+
+T = TypeVar("T", bound="Active")
+"""A type variable that is bound to the `Active` class"""
+
+
+class ActiveSequence(ABC, Generic[T], Sequence[T]):
+    def __init__(self, ctx: Context, parent: Optional[Active] = None):
+        """A sequence abstraction for any HTTP GET endpoint that returns a collection.
+
+        It lazily fetches data on demand, caches the results, and allows for standard sequence operations like indexing and slicing.
+
+        Parameters
+        ----------
+        ctx : Context
+            The context object that holds the HTTP session used for sending the GET request.
+        parent : Optional[Active], optional
+            An optional parent resource to establish a nested relationship, by default None.
+        """
+        super().__init__()
+        self._ctx = ctx
+        self._parent = parent
+        self._cache: Optional[List[T]] = None
+
+    @property
+    @abstractmethod
+    def _endpoint(self) -> str:
+        """
+        Abstract property to define the endpoint URL for the GET request.
+
+        Subclasses must implement this property to return the API endpoint URL that will
+        be queried to fetch the data.
+
+        Returns
+        -------
+        str
+            The API endpoint URL.
+        """
+        raise NotImplementedError()
+
+    @property
+    def _data(self) -> List[T]:
+        """
+        Fetch and cache the data from the API.
+
+        This method sends a GET request to the `_endpoint` and parses the response as a list of JSON objects.
+        Each JSON object is used to instantiate an item of type `T` using the class specified by `_cls`.
+        The results are cached after the first request and reused for subsequent access unless reloaded.
+
+        Returns
+        -------
+        List[T]
+            A list of items of type `T` representing the fetched data.
+        """
+        if self._cache:
+            return self._cache
+
+        response = self._ctx.session.get(self._endpoint)
+        results = response.json()
+        self._cache = [self._create_instance(**result) for result in results]
+        return self._cache
+
+    @overload
+    def __getitem__(self, index: int) -> T: ...
+
+    @overload
+    def __getitem__(self, index: slice) -> Sequence[T]: ...
+
+    def __getitem__(self, index):
+        return self._data[index]
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __str__(self) -> str:
+        return str(self._data)
+
+    def __repr__(self) -> str:
+        return repr(self._data)
+
+    @abstractmethod
+    def _create_instance(self, **kwargs) -> T:
+        """Create an instance of 'T'.
+
+        Returns
+        -------
+        T
+        """
+        raise NotImplementedError()
+
+    def reload(self) -> Self:
+        """
+        Clear the cache and reload the data from the API on the next access.
+
+        Returns
+        -------
+        ActiveSequence
+            The current instance with cleared cache, ready to reload data on next access.
+        """
+        self._cache = None
+        return self
+
+
+class ActiveFinderMethods(ActiveSequence[T], ABC, Generic[T]):
+    def __init__(self, ctx: Context, parent: Optional[Active] = None, uid: str = "guid"):
+        """Finder methods.
+
+        Provides various finder methods for locating records in any endpoint supporting HTTP GET requests.
+
+        Parameters
+        ----------
+        ctx : Context
+            The context containing the HTTP session used to interact with the API.
+        parent : Optional[Active], optional
+            Optional parent resource for maintaining hierarchical relationships, by default None
+        uid : str, optional
+            The default field name used to uniquely identify records, by default "guid"
+        """
+        super().__init__(ctx, parent)
+        self._uid = uid
+
+    def find(self, uid) -> T:
+        """
+        Find a record by its unique identifier.
+
+        Fetches a record either by searching the cache or by making a GET request to the endpoint.
+
+        Parameters
+        ----------
+        uid : Any
+            The unique identifier of the record.
+
+        Returns
+        -------
+        T
+
+        Raises
+        ------
+        ValueError
+            If no record is found.
+        """
+        # todo - add some more comments about this
+        if self._cache:
+            conditions = {self._uid: uid}
+            result = self.find_by(**conditions)
+        else:
+            endpoint = self._endpoint + uid
+            response = self._ctx.session.get(endpoint)
+            result = response.json()
+            result = self._create_instance(**result)
+
+        if not result:
+            raise ValueError(f"Failed to find instance where {self._uid} is '{uid}'")
+
+        return result
+
+    def find_by(self, **conditions: Any) -> Optional[T]:
+        """
+        Find the first record matching the specified conditions.
+
+        There is no implied ordering, so if order matters, you should specify it yourself.
+
+        Parameters
+        ----------
+        **conditions : Any
+
+        Returns
+        -------
+        Optional[T]
+            The first record matching the conditions, or `None` if no match is found.
+        """
+        return next((v for v in self._data if v.items() >= conditions.items()), None)
