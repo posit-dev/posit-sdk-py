f22f196aba715c8b249a2bf411443f4bb12dcbcb
Barret Schloerke <barret@posit.co>
2024-12-12 15:56:52 -0500

    unit and integration testing

---
+++ integration/tests/posit/connect/test_system.py
@@ -0,0 +1,71 @@
+import time
+from pathlib import Path
+
+from posit.connect import Client
+from posit.connect.system import SystemRuntimeCache
+from posit.connect.tasks import Task
+
+
+class TestSystem:
+    @classmethod
+    def setup_class(cls):
+        cls.client = Client()
+        assert cls.client.content.count() == 0
+        cls.content_item = cls.client.content.create(
+            name="Content_A",
+            default_r_environment_management=True,
+        )
+
+    # Copied from from integration/tests/posit/connect/test_packages.py
+    def _deploy_python_bundle(self):
+        path = Path("../../../resources/connect/bundles/example-flask-minimal/bundle.tar.gz")
+        path = (Path(__file__).parent / path).resolve()
+        bundle = self.content_item.bundles.create(str(path))
+        task = bundle.deploy()
+        task.wait_for()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.content_item.delete()
+        assert cls.client.content.count() == 0
+
+    def test_runtime_caches(self):
+        # Get current caches
+        caches: list[SystemRuntimeCache] = self.client.system.caches.runtime.find()
+        assert isinstance(caches, list)
+
+        # Remove all caches
+        for cache in caches:
+            assert isinstance(cache, SystemRuntimeCache)
+            task: Task = cache.destroy()
+            assert isinstance(task, Task)
+            task.wait_for()
+        assert len(self.client.system.caches.runtime.find()) == 0
+
+        # Deploy a new cache
+        self._deploy_python_bundle()
+
+        start_time = time.time()
+        continue_while = True
+        while continue_while:
+            now_time = time.time()
+            if now_time - start_time > 30:
+                raise TimeoutError("Timeout while waiting for cache to deploy")
+
+            jobs = self.content_item.jobs.fetch()
+            print("jobs:", jobs)
+            for job in jobs:
+                print("job:", job)
+                if str(job["status"]) == "0":
+                    # Stop for loop and restart while loop
+                    break
+            else:
+                # Only executed if the for loop did NOT break
+
+                # Break the while loop
+                continue_while = False
+
+        # Check if the cache is deployed
+        caches = self.client.system.caches.runtime.find()
+        print("caches:", caches)
+        assert len(caches) > 0
